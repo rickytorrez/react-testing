7ede6833de0cf8a32365681a5cb28d568f8e5c54
rickytorrez <torrez.ricky@ymail.com>
2020-11-24 11:31:45 -0500

    Congrats component

--- jotto/src/components/Congrats.js
+++ jotto/src/components/Congrats.js
@@ -3,15 +3,21 @@ import React from 'react';
 /**
  * Functional react component for congratulatory message.
  * @function
+ * @param {object} props = React props.
  * @returns {JSX.Element} - Rendered component (or null if `success` prop is true/false).
  */
-
-const Congrats = () => {
-	return (
-		<div>
-			<h1>Text</h1>
-		</div>
-	);
+const Congrats = (props) => {
+	if (props.success) {
+		return (
+			<div data-test='component-congrats'>
+				<span data-test='congrats-message'>
+					Congratulations! You guessed the word!
+				</span>
+			</div>
+		);
+	} else {
+		return <div data-test='component-congrats' />;
+	}
 };
 
 export default Congrats;
