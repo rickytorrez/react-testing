1c0d6d154cacfce241a7e89c018f21490649dfb3
rickytorrez <torrez.ricky@ymail.com>
2020-11-25 11:03:40 -0500

    Start tests for GuessedWords

--- jotto/src/components/GuessedWords.js
+++ jotto/src/components/GuessedWords.js
@@ -2,11 +2,15 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const GuessedWords = (props) => {
-	return (
-		<div>
-			<h1>Text</h1>
-		</div>
-	);
+	let contents;
+	if (props.guessedWords.length === 0) {
+		contents = (
+			<span data-test='guessed-instructions'>
+				Try to guess the secret word!
+			</span>
+		);
+	}
+	return <div data-test='component-guessed-words'>{contents}</div>;
 };
 
 GuessedWords.propTypes = {
