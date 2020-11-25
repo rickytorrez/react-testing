020342fdc110b422d8897c35912b25441f558dc8
rickytorrez <torrez.ricky@ymail.com>
2020-11-25 15:37:50 -0500

    spliting testing

---
+++ jotto-redux/src/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import './App.css';
+
+import GuessedWords from './components/GuessedWords';
+import Congrats from './components/Congrats';
+
+class App extends Component {
+	render() {
+		return (
+			<div className='container'>
+				<h1>Jotto</h1>
+				<Congrats success={true} />
+				<GuessedWords
+					guessedWords={[{ guessedWord: 'train', letterMatchCount: 3 }]}
+				/>
+			</div>
+		);
+	}
+}
+
+export default App;
