fca94ab50c3b5c8a9915ad9a05ba72aa80a7a10f
rickytorrez <torrez.ricky@ymail.com>
2020-11-27 13:44:00 -0500

    Input Field Test and  mocks for setCurrentGuess

--- jotto-context/src/components/Input.js
+++ jotto-context/src/components/Input.js
@@ -2,7 +2,25 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const Input = ({ secretWord }) => {
-	return <div data-test='component-input'></div>;
+	const [currentGuess, setCurrentGuess] = React.useState('');
+
+	return (
+		<div data-test='component-input'>
+			<form className='form-inline'>
+				<input
+					data-test='input-box'
+					className='mb-2 mx-sm3'
+					type='text'
+					placeholder='enter guess'
+					value={currentGuess}
+					onChange={(event) => setCurrentGuess(event.target.value)}
+				/>
+				<button data-test='submit-button' className='btn btn-primary'>
+					Submit
+				</button>
+			</form>
+		</div>
+	);
 };
 
 Input.propTypes = {
